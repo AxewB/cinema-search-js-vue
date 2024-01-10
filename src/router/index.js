37f4dbd32df582f68818a90ac834d616fd9edc4b
iaksenov <speedwow1@gmail.com>
2024-01-10 16:28:52 +0300

    first canvas of the app

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ const routes = [
   {
     path: '/',
     component: () => import('../views/HomePage.vue'),
-
+    
   },
 ]
 
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,6 +1,32 @@
 <template>
   <div>
-asd
+    
+    <v-sheet height="400px">
+      <v-img height="400px" cover src="https://wallpapers.com/images/hd/film-background-fwuwqptvgv1k0jco.jpg" alt="aboba">
+        <v-sheet class="d-flex 
+                      align-center
+                      justify-end 
+                      background-transparent"
+                height="50px">
+        <v-sheet class="flex-grow-1
+                        d-flex 
+                        justify-start 
+                        align-center
+                        background-transparent">
+          <v-btn>Home page</v-btn>
+          <v-btn>Recommendations</v-btn>
+        </v-sheet>
+        <v-sheet class="flex-grow-1
+                        d-flex 
+                        justify-end 
+                        align-center
+                        background-transparent">
+          <v-btn icon="mdi-heart"></v-btn>
+        </v-sheet>
+    </v-sheet>
+      </v-img>
+    </v-sheet>
+    and there will be list of films
   </div>
 </template>
 
@@ -9,7 +35,3 @@ asd
     name: 'HomePage'
   }
 </script>
-
-<style lang="scss" scoped>
-
-</style>
