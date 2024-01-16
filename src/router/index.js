192d04089e245b5073ecbf4ed1983288ce98d4cf
AxewB <speedwow1@gmail.com>
2024-01-17 01:14:31 +0300

    created base for routes

--- src/router/index.js
+++ src/router/index.js
@@ -4,9 +4,25 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   {
     path: '/',
+    name: 'home',
     component: () => import('../views/HomePage.vue'),
-    
+
   },
+  {
+    path: '/recommendations',
+    name: 'recommendations',
+    component: () => import('../views/RecommendationsPage.vue')
+  },
+  {
+    path: '/film/:id',
+    name: 'film',
+    children: [
+      {
+        path: '',
+        component() => import('../views/FilmPage.vue')
+      }
+    ]
+  }
 ]
 
 const router = createRouter({
---
+++ src/views/RecommendationsPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'RecommendationsPage'
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
