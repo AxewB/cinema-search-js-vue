78195a1a330cdc75765e830310508124a60b4d54
iaksenov <speedwow1@gmail.com>
2024-01-18 20:08:52 +0300

    moved store loader from home page to App

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,10 @@
 </template>
 
 <script setup>
-  //
+import { onBeforeMount } from 'vue';  
+import { useFilmStore } from './store/filmStore';
+const filmStore = useFilmStore()
+onBeforeMount(() => {
+  filmStore.filmsLoad();
+})
 </script>
