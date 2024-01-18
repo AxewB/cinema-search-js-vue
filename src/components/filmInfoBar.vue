5276e89d386567caaa5250727ec3f93c837cf033
iaksenov <speedwow1@gmail.com>
2024-01-18 20:09:54 +0300

    small bar to not repeat code

---
+++ src/components/filmInfoBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-sheet class="pa-2 d-flex flex-row bg-transparent">
+    <v-sheet class="bg-transparent text-disabled mr-2">
+      {{ title }}
+    </v-sheet>
+    <v-sheet class="bg-transparent">
+      {{ data }}
+    </v-sheet>
+  </v-sheet>
+</template>
+
+<script>
+export default {
+  name: "filmInfoBar",
+  props: {
+    'title': { type: String },
+    'data': { type: String }
+  } 
+}
+</script>
