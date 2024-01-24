ba4e2e9b7f798599b45d52dcb27208e794427f9b
iaksenov <speedwow1@gmail.com>
2024-01-24 15:39:22 +0300

    need to fix -feeling lucky- button

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -2,7 +2,7 @@
   <v-sheet class="background-transparent" width="100%">
         <v-toolbar rounded
                   elevation="10"
-                  class="d-flex">
+                  class="d-flex bg-transparent">
           <v-sheet class="bg-transparent" width="33%"/>
           <v-sheet class="bg-transparent d-flex justify-center" width="33%">
             <router-link :to="{name: 'home'}">
@@ -11,10 +11,7 @@
             <router-link :to="{name: 'recommendations'}">
               <v-btn exact>Recommendations</v-btn>
             </router-link>
-
-            <!-- todo: just sending to a random film page -->
-            <!-- also add 'to=link' -->
-            <router-link exact :to="{name: 'home'}">
+            <router-link>
               <v-btn>Feeling lucky</v-btn>
             </router-link>
           </v-sheet>
@@ -28,9 +25,15 @@
 </template>
 
 <script>
-  export default {
-    name: "NavigationBar"
+import { mapActions } from 'pinia';
+import { useFilmStore } from '@/store/filmStore';
+export default {
+  name: "NavigationBar",
+  methods: {
+    ...mapActions(useFilmStore, ['getRandomFilmId'])
   }
+}
+  
 </script>
 
 <style lang="scss" scoped>
