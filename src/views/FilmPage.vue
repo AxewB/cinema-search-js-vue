351dc2a5bcb695cc2bccc83d9e29a80c6c1ff7fa
iaksenov <speedwow1@gmail.com>
2024-01-25 18:41:07 +0300

    added possibility to rate film

--- src/views/FilmPage.vue
+++ src/views/FilmPage.vue
@@ -5,6 +5,8 @@
                   pa-2" 
             width="100%">
     <v-sheet class="bg-transparent" width="1200px" rounded >
+      <NavigationBar/>
+
       <VImg :src="film.poster.url" 
             height="100vh" 
             cover 
@@ -17,12 +19,9 @@
                    @click="$router.go(-1)">
               Back
             </v-btn>
-            <v-btn icon
-                   rounded="lg"
-                   @click="addFilmToFavourites()"
-                   :color="isInFavourites ? 'deep-purple-darken-1' : ''">
-              <VIcon icon="mdi-heart" />
-            </v-btn>
+            <v-sheet class="bg-transparent flex-grow-1">
+            </v-sheet>  
+            
           </v-sheet>
           <v-sheet class="bg-transparent 
                           d-flex 
@@ -37,7 +36,46 @@
                     cover 
                     width="270px"/>
               
-              <VDivider class="ma-5"/>
+              <VDivider class="ma-2"/>
+              <VBtn  v-if="!userStore.isExists(filmId)" 
+                      width="100%" 
+                      @click="addFilmToUserList()" text="Add to list"/>
+              <v-sheet v-else class="d-flex flex-column bg-transparent">
+                <v-sheet class="d-flex flex-row bg-transparent">
+                  <VSelect  width="100%" 
+                            :items="userStore.lists" 
+                            v-model="userStore.films[filmId].status" 
+                            hide-details
+                            class="flex-grow-1 mr-2 text-capitalize"
+                            density="comfortable"
+                            variant="solo"/>
+                  
+                  
+                </v-sheet>
+                <v-sheet width="100%" class="bg-transparent mt-2 d-flex">
+                  <v-btn icon
+                        rounded
+                        variant="tonal"
+                        @click="addFilmToFavourites()"
+                        :color="isInFavourites ? 'primary' : ''"
+                        class="mr-2 flex-grow-1">
+                    <VTooltip text="Add to favourites" activator="parent" location="top"></VTooltip>
+                    <VIcon icon="mdi-heart" />
+                  </v-btn>
+                  <v-btn icon rounded @click="removeFilmFromList()" variant="tonal" class="mr-2" color="error">
+                    <VTooltip text="Remove" activator="parent" location="top"></VTooltip>
+                    <VIcon icon="mdi-delete"></VIcon>
+                  </v-btn>  
+                </v-sheet>
+                <v-sheet class="d-flex flex-row bg-transparent justify-center" width="100%">
+                  <VRating
+                    v-model="userStore.films[this.$route.params.id].rating"
+                    hover
+                    half-increments/>
+                </v-sheet>
+              </v-sheet>
+              
+              <VDivider class="ma-2"/>
               <v-list rounded >
                 <v-list-subheader>WHERE TO WATCH</v-list-subheader>
                 <v-list max-height="250px" class="overflow-auto">
@@ -67,10 +105,11 @@
               <v-sheet class="text-body-1 bg-transparent pa-2 mt-10" width="100%" >
                 {{ film.description }}
               </v-sheet>
-              <VDivider class="ma-5"/>
-              <filmInfoBar v-if="film.year" :title="'Year'" :data="film.year"/>
+              
+              <VDivider class="ma-2"/>
+              <filmInfoBar v-if="film.year" :title="'Year'" :data="film.year.toString()"/>
               <filmInfoBar v-if="film.alternativeName" :title="'Alternative name'" :data="film.alternativeName"/>
-              <filmInfoBar v-if="film.movieLength" :title="'Length'" :data="film.movieLength"/>
+              <filmInfoBar v-if="film.movieLength" :title="'Length'" :data="film.movieLength.toString()"/>
               <filmInfoBar v-if="film.type" :title="'Type'" :data="film.type"/>
               <filmInfoBar v-if="otherNames" :title="'Other names'" :data="otherNames"/>
             </v-sheet>
@@ -86,23 +125,34 @@ import { mapStores } from 'pinia';
 import { useFilmStore } from '@/store/filmStore';
 import { useUserStore } from '@/store/userStore';
 import filmInfoBar from '@/components/filmInfoBar.vue'
+import NavigationBar from '@/components/NavigationBar.vue';
 export default {
   name: "FilmPage", 
   components: {
-    filmInfoBar
+    filmInfoBar,
+    NavigationBar
   },
   methods: {
     addFilmToFavourites() {
-      this.userStore.toggleFilmInFavourites(this.$route.params.id) 
+      this.userStore.toggleFilmFavourite(this.filmId, this.film) 
+    },
+    addFilmToUserList() {
+      this.userStore.addFilmToProfile(this.filmId, this.film)
+    },
+    removeFilmFromList() {
+      this.userStore.removeFilmFromProfile(this.filmId)
     }
   },
   computed: {
     ...mapStores(useFilmStore, useUserStore),
+    filmId() {
+      return this.$route.params.id
+    },
     film() {
-      return this.filmStore.films.find(film => film.id == this.$route.params.id)
+      return this.filmStore.films.find(film => film.id == this.filmId)
     },
     isInFavourites() {
-      return this.userStore.isExists(this.$route.params.id)
+      return this.userStore.isInFavourites(this.filmId)
     },
     otherNames() {
       let names = '';
